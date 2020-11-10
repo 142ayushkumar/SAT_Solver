762eaca65997e7ad7fd897d3929636428b2ffb0d
Kumar Ayush <142ayushkumar@gmail.com>
2020-11-10 23:41:59 +0530

    Removed some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 	// step 1 and 3 from pseudocode, 4 is also included in the elseif condition
 	if(set_literal != 0)
 	{
+		curr_assignment[abs(set_literal)] = set_literal;
 		// remove clauses containing literal
 		for(int i=0;i<(int)clauses.size();i++)
 		{
@@ -34,7 +35,7 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 			else if(clauses[i].find(-set_literal) != clauses[i].end())
 			{
 				// put this in store_index and remove this literal from this clause
-				store_index.push_back(i);
+				store_index.push_back(-i);
 				clauses[i].erase(-set_literal);
 				if(clauses[i].size() == 0) // it must be true for sat otherwise this is unsat for assigned value yet
 					return false;
@@ -54,7 +55,7 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 	int unit_literal = 0;
 	for(int i=0;i<(int)clauses.size();i++)
 	{
-		if(clauses[i].size() == 1)
+		if(clauses[i].size() == 1 and already_true[i] == false)
 		{
 			//unit literal
 			unit_literal = *clauses[i].begin();
@@ -67,6 +68,7 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 		bool found = false, positive = true;
 		for(int i=0;i<(int)clauses.size();i++)
 		{
+			if(already_true[i]) continue;
 			if(clauses[i].find(j) != clauses[i].end())
 			{
 				if(!found)
@@ -79,7 +81,7 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 					break;
 				}
 			}
-			else if(clauses[i].find(-j) != clauses[i].end())
+			if(clauses[i].find(-j) != clauses[i].end())
 			{
 				if(!found)
 				{
@@ -100,19 +102,48 @@ bool dpll(vector<set<int>>& clauses, vector<bool>& already_true, int set_literal
 
 	// step 6, choosing a clause and assigning it
 	int unassigned = 0;
-	for(int i=0;i<(int)clauses.size();i++)
+	bool satisfied_all = true;
+	for(int i=1;i<=n_variables;i++)
 	{
 		if(clauses[i].size())
 		{
-			unassigned = *clauses[i].begin();
+			satisfied_all = false;
+		}
+		if(curr_assignment.find(i) == curr_assignment.end())
+		{
+			unassigned = i;
 			break;
+		}
+	}
+	if(unassigned == 0)
+	{
+		if(satisfied_all) return true;
+		else 
+		{
+			return false;
 		}
 	}
 	if(dpll(clauses, already_true, -unassigned))
 	{
 		return true;
 	}
-	else return dpll(clauses, already_true, unassigned);
+	else 
+	{
+		// remove the current assignmeent
+		curr_assignment.erase(set_literal);
+		// turn all those assignment turned false by this
+		for(int i:turned_true)
+			already_true[i] = false;
+		// add this literal in all the clauses in which true was assigned
+		for(int i:store_index)
+		{
+			if(i < 0)
+				clauses[-i].insert(-set_literal);
+			else
+				clauses[i].insert(set_literal);
+		}
+		return dpll(clauses, already_true, unassigned);
+	}
 }
 
 int main()
@@ -135,4 +166,13 @@ int main()
 	}
 	vector<bool> already_true(n_clauses, 0);
 	bool found = dpll(clauses, already_true);
+	if(found)
+	{
+		cout << "SAT\n";
+		cout << "Curr_asignment size is " << curr_assignment.size() << "\n";
+		for(auto i:curr_assignment)
+			cout << i.first << " " << i.second << "\n";
+		cout <<"\n";
+	}
+	else cout << "UNSAT\n";
 }
